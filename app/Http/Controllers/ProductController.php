9b4205e1016ca49b2158392237640ea0357d69a4
Eduardo9494 <1433341@senati.pe>
2023-11-01 18:00:25 -0500

    Creamos el productoSold y tambien migramos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\ProductSold; 
+use App\Models\ProductSold;  
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
