2af0901ebda033f219feea0d23f262e8991919db
Eduardo9494 <1433341@senati.pe>
2023-11-03 15:54:42 -0500

    Hacemos ultimos retoques a nuestro trabajo final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="../assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="../assets/img/AdminLTELogo.jfif" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Tiendita 1.0</span>
     </a>
  
@@ -44,7 +44,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../assets/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
+          <img src="../assets/img/logo.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">{{ Auth::user()->name }}</a>
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -29,7 +29,7 @@
               <div class="card-body box-profile">
                 <div class="text-center">
                   <img class="profile-user-img img-fluid img-circle"
-                       src="../assets/img/avatar4.png"
+                       src="../assets/img/logo.jpg"
                        alt="User profile picture">
                 </div>
  
